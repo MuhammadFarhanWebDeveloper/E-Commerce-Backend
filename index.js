8f7323ab41f75b12201618070e1264456ef2a0d8
Farhan <muhammadfarhan3789076@gmail.com>
2024-09-18 21:24:36 +0500

    initial

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
-import auth from "./views/auth.js"
-import product from "./views/product.js"
+import auth from "./views/auth.js";
+import product from "./views/product.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -16,6 +16,9 @@ app.use(express.json());
 app.use("/api/auth", auth);
 app.use("/api/product", product);
 
+app.get("/", (req, res) => {
+  res.json({ woring: true });
+});
 
 app.listen(PORT, () => {
   console.log(`Your app is running on http://localhost:${PORT}`);
