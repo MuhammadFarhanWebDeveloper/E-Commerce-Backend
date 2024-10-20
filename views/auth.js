eda3d8f906d53e0d7e5f6b08f5625ef90f6d9ad6
Farhan <muhammadfarhan3789076@gmail.com>
2024-10-20 15:20:08 +0500

    fixing error

--- views/auth.js
+++ views/auth.js
@@ -17,11 +17,9 @@ import {
 import isTokenSent from "../middleware/isTokenSent.js";
 import isTokenVerified from "../middleware/isTokenVerified.js";
 import resetPasswordToken from "../middleware/ResetPasswordToken.js";
-import { isSeller } from "../middleware/isUserSeller.js";
 import upload from "../middleware/multer.js";
 const router = express.Router();
 
-// Registration validation
 const registerFieldsValidation = [
   body("firstName")
     .isLength({ min: 4, max: 20 })
@@ -54,7 +52,6 @@ const loginFieldsValidation = [
     .isLength({ min: 4 })
     .withMessage("Password must be at least 4 characters long."),
 ];
-// Validation middleware
 const userUpdateValidation = [
   body("firstName")
     .optional()
@@ -75,6 +72,8 @@ const userUpdateValidation = [
     .withMessage("Please provide a valid phone number."),
 ];
 const sendOTPValidation = [body("email", "Enter a valid email").isEmail()];
+
+
 router.post("/send-otp", sendOTPValidation, sendOTP);
 router.post("/verify-otp", isTokenSent, verifyOTP);
 router.post("/register", isTokenVerified, registerFieldsValidation, register);
@@ -84,7 +83,7 @@ router.post("/reset-password", resetPasswordToken, resetPassword);
 
 router.post("/logout", isUserLoggedIn, logout);
 router.post("/getuser", isUserLoggedIn, getUser);
-router.put("/update-user", upload.single("profilePicture"), updateUserInfo);
+router.put("/update-user",isUserLoggedIn , upload.single("profilePicture"), updateUserInfo);
 router.post("/become-seller", isUserLoggedIn, upload.single("logo"), becomeSeller);
 
 router.delete('/delete-user', deleteUser);
