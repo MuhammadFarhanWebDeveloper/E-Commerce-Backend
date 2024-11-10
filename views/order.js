b48089bb62b84bab611484302fbd931f1f1c964c
Farhan <muhammadfarhan3789076@gmail.com>
2024-11-10 16:11:15 +0500

    fixing error

--- views/order.js
+++ views/order.js
@@ -1,18 +1,10 @@
 import express from "express";
 import isUserLoggedIn from "../middleware/isUserLoggedIn.js";
 import { isSeller } from "../middleware/isUserSeller.js";
-import {
-  buyProduct,
-  deleteOrder,
-  getOrder,
-  getOrders,
-} from "../controllers/order.controller.js";
+import { buyProduct } from "../controllers/order.controller.js";
 
 const order = express.Router();
 
 order.post("/buy/:id", isUserLoggedIn, buyProduct);
-order.get("/get-order/:id", isUserLoggedIn, isSeller, getOrder);
-order.delete("/delete/:id", isUserLoggedIn, isSeller, deleteOrder);
-order.get("/get-orders", isUserLoggedIn, isSeller, getOrders);
 
 export default order;
